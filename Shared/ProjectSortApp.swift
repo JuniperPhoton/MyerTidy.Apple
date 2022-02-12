fd34b3cdd46cbc54718a0ffde55afe031a5c3864
juniperphoton <dengweichao@hotmail.com>
2022-02-12 19:07:40 +0800

    Initial Commit

---
+++ Shared/ProjectSortApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectSortApp.swift
+//  Shared
+//
+//  Created by juniperphoton on 2022/2/12.
+//
+
+import SwiftUI
+
+@main
+struct ProjectSortApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
